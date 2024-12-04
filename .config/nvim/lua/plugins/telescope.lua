b5daf8177722590b5ea7096dad80b4b99fa6394a
Alan Cunha <alanalegredacunha@gmail.com>
2024-12-04 20:21:51 -0300

    setup kitty term

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -62,7 +62,18 @@ return {
             prompt_position = 'top', -- search bar at the top
           },
         },
-        -- pickers = {}
+        pickers = {
+          live_grep = {
+            file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+            additional_args = function(_)
+              return { '--hidden' }
+            end,
+          },
+          find_files = {
+            file_ignore_patterns = { 'node_modules', '.git', '.venv' },
+            hidden = true,
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
