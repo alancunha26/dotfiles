0b8dbf7dcef2acc1d0d521f37d79c1afe7739e9e
Alan Cunha <alanalegredacunha@gmail.com>
2024-12-25 10:19:20 -0300

    update
    # Conflicts:
    #	.config/nvim/lazy-lock.json

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -1,12 +1,16 @@
 -- https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/114
 return {
   'MeanderingProgrammer/render-markdown.nvim',
+  enabled = false,
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     { 'nvim-tree/nvim-web-devicons', enbled = vim.g.have_nerd_font },
   },
   opts = {
     sign = {
+      enabled = false,
+    },
+    pipe_table = {
       enabled = false,
     },
     -- link = {
